becccc1aad1328cfa5e36c90eddd7573bf8b9ca1
Shinjiro Itagaki <ishinjiroster@gmail.com>
2020-01-21 00:34:28 +0900

    SITableのパース処理を大幅に修正

--- src/SITables.hs
+++ src/SITables.hs
@@ -25,74 +25,68 @@ import qualified BytesReader.HolderIO as HolderIO
 import qualified SITables.Header1 as Header1
 import Common(EmptyExist(..))
 import qualified Parser.Result as Result
-import Parser.Result((>>===))
+-- import Parser.Result((>>===))
 
-data Data =
-  Other
-  | Null
-  | IsBAT  SITables.BAT.Data
-  | IsBIT  SITables.BIT.Data
-  | IsEIT  SITables.EIT.Data
-  | IsLDT  SITables.LDT.Data
-  | IsNBIT SITables.NBIT.Data
-  | IsNIT  SITables.NIT.Data
-  | IsPCAT SITables.PCAT.Data
-  | IsRST  SITables.RST.Data
-  | IsSDT  SITables.SDT.Data
-  | IsST   SITables.ST.Data
-  | IsTDT  SITables.TDT.Data
-  | IsTOT  SITables.TOT.Data
+type Callback d state = (d -> state -> IO state)
 
-data Constructor =
-  ConsBAT (SITables.BAT.Data -> Data)
-  | ConsBIT  (SITables.BIT.Data -> Data)
-  | ConsEIT  (SITables.EIT.Data -> Data)
-  | ConsLDT  (SITables.LDT.Data -> Data)
-  | ConsNBIT (SITables.NBIT.Data -> Data)
-  | ConsNIT  (SITables.NIT.Data -> Data)
-  | ConsPCAT (SITables.PCAT.Data -> Data)
-  | ConsRST  (SITables.RST.Data -> Data)
-  | ConsSDT  (SITables.SDT.Data -> Data)
-  | ConsST   (SITables.ST.Data -> Data)
-  | ConsTDT  (SITables.TDT.Data -> Data)
-  | ConsTOT  (SITables.TOT.Data -> Data)
+data Callbacks state = MkCallbacks {
+  _cb_BAT  :: Maybe (Callback SITables.BAT.Data  state ),
+  _cb_BIT  :: Maybe (Callback SITables.BIT.Data  state ),
+  _cb_EIT  :: Maybe (Callback SITables.EIT.Data  state ),
+  _cb_LDT  :: Maybe (Callback SITables.LDT.Data  state ),
+  _cb_NBIT :: Maybe (Callback SITables.NBIT.Data state ),
+  _cb_NIT  :: Maybe (Callback SITables.NIT.Data  state ),
+  _cb_PCAT :: Maybe (Callback SITables.PCAT.Data state ),
+  _cb_RST  :: Maybe (Callback SITables.RST.Data  state ),
+  _cb_SDT  :: Maybe (Callback SITables.SDT.Data  state ),
+  _cb_ST   :: Maybe (Callback SITables.ST.Data   state ),
+  _cb_TDT  :: Maybe (Callback SITables.TDT.Data  state ),
+  _cb_TOT  :: Maybe (Callback SITables.TOT.Data  state )
+  }
 
-cBAT = ConsBAT IsBAT
-cBIT = ConsBIT IsBIT
-cEIT = ConsEIT IsEIT
-cLDT = ConsLDT IsLDT
-cNBIT = ConsNBIT IsNBIT
-cNIT = ConsNIT IsNIT
-cPCAT = ConsPCAT IsPCAT
-cRST = ConsRST IsRST
-cSDT = ConsSDT IsSDT
-cST  = ConsST IsST
-cTDT = ConsTDT IsTDT
-cTOO = ConsTOT IsTOT
+parseIO :: (HolderIO.Class bh) => bh -> state -> Callbacks state -> IO state
+parseIO bh state callbacks = do
+  (res_header1,bh2) <- Header1.parseIO bh
+  case res_header1 of
+    Result.Parsed x -> _parseIO bh2 x state callbacks  -- 終了
+    _               -> return state -- ヘッダのパースエラーにつき続行不能で終了
 
-parseIO :: (HolderIO.Class bh) => bh -> [Constructor] -> IO (ParseResult Data, bh)
-parseIO bh []     = return (Result.NotMatch, bh)
-parseIO bh (x:xs) = do
-  rtn@(res,bh2) <- (Header1.parseIO bh >>=== (\(header1,bh2) -> _parseIO bh2 x header1))
-  case res of
-    Result.Parsed x -> return rtn -- 終了
-    Result.NotMatch -> parseIO bh2 xs -- マッチなしで次に進む
-    _               -> parseIO bh2 xs -- パースエラーにつきデータは破棄して終了
 
-_parseIO :: (HolderIO.Class bh) => bh -> Constructor -> Header1.Data -> IO (ParseResult Data, bh)
-_parseIO bh cons header1 =
-  case cons of
-    ConsBAT  f -> impl' f
-    ConsBIT  f -> impl' f
-    ConsEIT  f -> impl' f
-    ConsLDT  f -> impl' f
-    ConsNBIT f -> impl' f
-    ConsNIT  f -> impl' f
-    ConsPCAT f -> impl' f
-    ConsRST  f -> impl' f
-    ConsSDT  f -> impl' f
-    ConsST   f -> impl' f
-    ConsTDT  f -> impl' f
-    ConsTOT  f -> impl' f
+_parseIO :: (HolderIO.Class bh) => bh -> Header1.Data -> state -> Callbacks state -> IO state
+_parseIO bh header1 state callbacks =
+  case callbacks of
+    (MkCallbacks {_cb_BAT = Just f}) -> impl' f $ callbacks{_cb_BAT = Nothing}
+    (MkCallbacks {_cb_BIT = Just f}) -> impl' f $ callbacks{_cb_BIT = Nothing}
+    (MkCallbacks {_cb_EIT = Just f}) -> impl' f $ callbacks{_cb_EIT = Nothing}
+    (MkCallbacks {_cb_LDT = Just f}) -> impl' f $ callbacks{_cb_LDT = Nothing}
+    (MkCallbacks {_cb_NBIT= Just f}) -> impl' f $ callbacks{_cb_NBIT= Nothing}
+    (MkCallbacks {_cb_NIT = Just f}) -> impl' f $ callbacks{_cb_NIT = Nothing}
+    (MkCallbacks {_cb_PCAT= Just f}) -> impl' f $ callbacks{_cb_PCAT= Nothing}
+    (MkCallbacks {_cb_RST = Just f}) -> impl' f $ callbacks{_cb_RST = Nothing}
+    (MkCallbacks {_cb_SDT = Just f}) -> impl' f $ callbacks{_cb_SDT = Nothing}
+    (MkCallbacks {_cb_ST  = Just f}) -> impl' f $ callbacks{_cb_ST  = Nothing}
+    (MkCallbacks {_cb_TDT = Just f}) -> impl' f $ callbacks{_cb_TDT = Nothing}
+    (MkCallbacks {_cb_TOT = Just f}) -> impl' f $ callbacks{_cb_TOT = Nothing}
+    _                                -> return state
   where
-    impl' f = Base.parseIO mkEmpty header1 bh >>= (\(x,y) -> return (mapParseResult f x, y))
+    impl' f' callbacks2 = do
+      (res,bh2) <- Base.parseIO mkEmpty header1 bh
+      case res of
+        Result.Parsed x -> f' x state -- 更新した状態を返して終了
+        Result.NotMatch -> _parseIO bh2 header1 state callbacks2 -- マッチしなかったので次に進む
+        _               -> return state -- エラー発生につき終了
+
+emptyCallbacks = MkCallbacks {
+  _cb_BAT  = Nothing,
+  _cb_BIT  = Nothing,
+  _cb_EIT  = Nothing,
+  _cb_LDT  = Nothing,
+  _cb_NBIT = Nothing,
+  _cb_NIT  = Nothing,
+  _cb_PCAT = Nothing,
+  _cb_RST  = Nothing,
+  _cb_SDT  = Nothing,
+  _cb_ST   = Nothing,
+  _cb_TDT  = Nothing,
+  _cb_TOT  = Nothing
+  }
